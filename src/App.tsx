48637e2017bccceb06789c4a97b4659412851dda
Mário Monteiro <marioammonteiro@gmail.com>
2022-01-17 22:47:04 +0000

    updated to ionic 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { setupIonicReact, IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
@@ -26,6 +26,11 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+// required for ionic6 else page opens blank
+setupIonicReact({
+  mode: 'md'
+});
 
 const App: React.FC = () => (
   <IonApp>
